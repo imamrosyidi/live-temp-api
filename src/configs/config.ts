dc4596d0268445f3cdaf3e035af15f4e156125b8
rosyidi-jubelio <imam.rosyidi@jubelio.com>
2024-10-26 21:33:21 +0700

    style: refactor config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,12 +3,12 @@ import "dotenv/config";
 class Config {
   readonly APP_PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
   readonly DB_USER = process.env.DB_USER || "db_user";
+  readonly DB_PASSWORD = process.env.DB_PASSWORD || "db_password";
+  readonly DB_HOST = process.env.DB_HOST || "db_host";
+  readonly DB_NAME = process.env.DB_NAME || "db_name";
   readonly DB_PORT = process.env.DB_PORT
     ? parseInt(process.env.DB_PORT, 10)
     : 8000;
-  readonly DB_NAME = process.env.DB_NAME || "db_name";
-  readonly DB_HOST = process.env.DB_HOST || "db_host";
-  readonly DB_PASSWORD = process.env.DB_PASSWORD || "db_password";
 }
 
 const config = new Config();
