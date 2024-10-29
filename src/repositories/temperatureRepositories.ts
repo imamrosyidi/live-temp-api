8ee9a86c6ea6174545915b3b72dd0071d3f8b699
imamrosyidi <im.rosyidi15@gmail.com>
2024-10-29 14:24:54 +0700

    fix: limit the defautl list data

--- src/repositories/temperatureRepositories.ts
+++ src/repositories/temperatureRepositories.ts
@@ -25,7 +25,7 @@ export const getTemperaturesByTimeRange = async (
 
   const query = start
     ? `SELECT * FROM temperatures WHERE created_at BETWEEN $1 AND $2 ORDER BY created_at ASC`
-    : `SELECT * FROM temperatures WHERE created_at >= NOW() - INTERVAL '24 hours' ORDER BY created_at ASC`;
+    : `SELECT * FROM temperatures WHERE created_at >= NOW() - INTERVAL '24 hours' ORDER BY created_at ASC LIMIT 100`;
 
   const values = start ? [start, end || now] : [];
 
