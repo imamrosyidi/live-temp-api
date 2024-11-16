bf9b555152dd9c65aa2b178971a5b136e553f69d
imamrosyidi <im.rosyidi15@gmail.com>
2024-11-17 01:12:38 +0700

    fix: return io instance when initializing

--- src/initializers/socket.ts
+++ src/initializers/socket.ts
@@ -3,13 +3,16 @@ import server from "./server";
 
 export let io: Server;
 
-export const initializeSocket = () => {
-  io = new Server(server, { cors: { origin: "*" } });
+export const initializeSocket = (): Server => {
+  if (!io) {
+    io = new Server(server, { cors: { origin: "*" } });
 
-  io.on("connection", (socket) => {
-    console.log("A user connected");
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
+    io.on("connection", (socket) => {
+      console.log("A user connected");
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+      });
     });
-  });
+  }
+  return io;
 };
